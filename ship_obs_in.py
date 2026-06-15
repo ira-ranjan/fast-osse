229d0c333e146c80ed320f22bf4f91b0269b86e7
Ira Ranjan <iraxranjan@gmail.com>
2026-06-15 09:05:01 -0600

    script for generating ship obs metadata

---
+++ ship_obs_in.py
@@ -0,0 +1,32 @@
+# %%
+import datetime as dt
+import obs_to_obs_seq_in.py as obsin
+import numpy as np
+
+# %%
+def interpolated_list(lat0, lon0, t0, lat1, lon1, t1, number):
+    ts0 = t0.timestamp()
+    ts1 = t1.timestamp()
+    coords = np.linspace([lat0, lon0], [lat1, lon1], number)
+    times = np.linspace([ts0], [ts1], number)
+    points =[]
+    for ii in range(len(times)):
+        points.append([float(coords[ii][0]), float(coords[ii][1]), dt.datetime.fromtimestamp(times[ii][0])])
+    return points
+
+# %%
+def ship_data(list_ship, lat0, lon0, t0, lat1, lon1, t1, number, vert):
+    vert_unit = 'height (m)'
+    type = 'RADIOSONDE TEMPERATURE'
+    metadata = []
+    external_FO = []
+    obs_err_var = 0.01
+    locations = interpolated_list(lat0, lon0, t0, lat1, lon1, t1, number)
+    for coords in locations:
+            obsin.add_obs_to_list(list_ship, coords[0], coords[1], vert, vert_unit, type, coords[2], obs_err_var, metadata, external_FO)
+    return list_ship
+
+# %%
+
+
+
